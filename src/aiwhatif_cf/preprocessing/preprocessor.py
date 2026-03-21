56dfedf08a915a0c0c311c8283f362deac382939
Jesper Anteryd <jesper.anteryd@proton.me>
2026-03-21 14:22:07 +0100

    added datasets for each target (data/05_single_target). testing with Gridsearch in notebooks for RF and several NB models.

--- src/aiwhatif_cf/preprocessing/preprocessor.py
+++ src/aiwhatif_cf/preprocessing/preprocessor.py
@@ -5,7 +5,7 @@ class DataPreprocessor:
     def __init__(self, outcome_cols):
         self.outcome_cols = outcome_cols
 
-    def split_features_and_targets(self, df: pd.DataFrame):
+    def split_X_and_targets(self, df: pd.DataFrame):
         X = df.drop(columns=self.outcome_cols)
         y_dict = {t: df[t] for t in self.outcome_cols}
         return X, y_dict
